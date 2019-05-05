95d3a98aea80651533d69645063806a0d475024a
maizitoday <maizi_today@sina.com>
2019-05-05 23:57:26 +0800

    test项目

---
+++ src/main/java/com/maizi/App.java
@@ -0,0 +1,17 @@
+package com.maizi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+
+
+    
+}
